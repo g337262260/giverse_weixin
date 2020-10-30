95dba69aa12253e980b392be7af2ba1c1b35fb19
GuoW <g337262260@163.com>
2020-10-30 16:24:03 +0800

    version1.0.0

--- giverse/pages/index/index.js
+++ giverse/pages/index/index.js
@@ -9,6 +9,9 @@ Page({
     that.data.setInter = setInterval(function(){
       that.timeFn()
     },1000)
+    that.data.setInter = setInterval(function(){
+      that.timeFn1()
+    },1000)
   },
   endTime:function(){
     var that = this;
@@ -21,7 +24,7 @@ Page({
     this.endTime()
   },
   timeFn(){
-    var strTime = '2017-12-31 00:00:00'
+    var strTime = '2020-2-15 00:00:00'
     var dateBegin = new Date(Date.parse(strTime.replace(/-/g, "/")))
     var dateEnd = new Date()
     var dateDiff = dateEnd.getTime()-dateBegin.getTime()
@@ -34,5 +37,20 @@ Page({
     var seconds = Math.round(level3/1000)
     var timeString = "" + dayDiff + " · " + hours + " · " + minutes +" · "+seconds
     this.setData({msg:timeString})
+  },
+  timeFn1(){
+    var strTime = '2020-4-23 00:00:00'
+    var dateBegin = new Date(Date.parse(strTime.replace(/-/g, "/")))
+    var dateEnd = new Date()
+    var dateDiff = dateEnd.getTime()-dateBegin.getTime()
+    var dayDiff = Math.floor(dateDiff/(24*3600*1000))
+    var level1 = dateDiff%(24*3600*1000)
+    var hours = Math.floor(level1/(3600*1000))
+    var level2 = level1%(3600*1000)
+    var minutes = Math.floor(level2/(60*1000))
+    var level3 = level2%(60*1000)
+    var seconds = Math.round(level3/1000)
+    var timeString = "" + dayDiff + " · " + hours + " · " + minutes +" · "+seconds
+    this.setData({msg1:timeString})
   }
 })
